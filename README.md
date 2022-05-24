7b2136a5b97d7e6f49487bb1070c389cc46ac65c
chenjie <orange.tree@aliyun.com>
2022-05-24 18:18:55 +0800

    init

---
+++ README.md
@@ -0,0 +1,32 @@
+## 简介
+
+Vue3 + TypeScript + Naive UI + Vite
+
+## 使用
+
+* 获取项目代码
+
+```bash
+git clone https://github.com/jekip/naive-ui-admin.git
+```
+
+* 安装依赖
+
+```bash
+cd naive-ui-admin
+
+yarn install
+
+```
+
+* 运行
+
+```bash
+yarn dev
+```
+
+* 打包
+
+```bash
+yarn build
+```
---
+++ src/views/about/index.vue
@@ -0,0 +1,85 @@
+<template>
+  <div>
+    <n-card
+      :bordered="false"
+      title="项目信息"
+      class="mt-4 proCard"
+      size="small"
+      :segmented="{ content: 'hard' }"
+    >
+      <n-descriptions bordered label-placement="left" class="py-2">
+        <n-descriptions-item label="版本">
+          <n-tag type="info"> {{ version }} </n-tag>
+        </n-descriptions-item>
+        <n-descriptions-item label="最后编译时间">
+          <n-tag type="info"> {{ lastBuildTime }} </n-tag>
+        </n-descriptions-item>
+        <n-descriptions-item label="预览地址">
+          <div class="flex items-center">
+            <a href="" class="py-2" target="_blank"
+              >查看预览地址</a
+            >
+          </div>
+        </n-descriptions-item>
+        <n-descriptions-item label="Github">
+          <div class="flex items-center">
+            <a href="" class="py-2" target="_blank"
+              >查看Git地址</a
+            >
+          </div>
+        </n-descriptions-item>
+      </n-descriptions>
+    </n-card>
+
+    <n-card
+      :bordered="false"
+      title="开发环境依赖"
+      class="mt-4 proCard"
+      size="small"
+      :segmented="{ content: 'hard' }"
+    >
+      <n-descriptions bordered label-placement="left" class="py-2">
+        <n-descriptions-item v-for="item in devSchema" :key="item.field" :label="item.field">
+          {{ item.label }}
+        </n-descriptions-item>
+      </n-descriptions>
+    </n-card>
+
+    <n-card
+      :bordered="false"
+      title="生产环境依赖"
+      class="mt-4 proCard"
+      size="small"
+      :segmented="{ content: 'hard' }"
+    >
+      <n-descriptions bordered label-placement="left" class="py-2">
+        <n-descriptions-item v-for="item in schema" :key="item.field" :label="item.field">
+          {{ item.label }}
+        </n-descriptions-item>
+      </n-descriptions>
+    </n-card>
+  </div>
+</template>
+
+<script lang="ts" setup>
+  export interface schemaItem {
+    field: string;
+    label: string;
+  }
+
+  const { pkg, lastBuildTime } = __APP_INFO__;
+  const { dependencies, devDependencies, name, version } = pkg;
+
+  const schema: schemaItem[] = [];
+  const devSchema: schemaItem[] = [];
+
+  Object.keys(dependencies).forEach((key) => {
+    schema.push({ field: key, label: dependencies[key] });
+  });
+
+  Object.keys(devDependencies).forEach((key) => {
+    devSchema.push({ field: key, label: devDependencies[key] });
+  });
+</script>
+
+<style lang="less" scoped></style>
